a19648a961e44cd301ea45218a124b923fe8b046
David McElhinney <davidmcelhinney10@hotmail.com>
2020-02-26 12:34:53 +0000

    Test

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -82,7 +82,7 @@ class PaymentController extends Controller
           // $paymentMethods->data[0]->card->last4
 
         // Return data.
-        return response()->json($paymentMethods);
+        return response()->json($paymentMethods->data[0]->card->last4);
     }
 
     /**

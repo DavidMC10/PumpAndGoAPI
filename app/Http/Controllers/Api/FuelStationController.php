405e909ac628cc2c2567b81ff9122412e2744f83
David Mc <davidmcelhinney10@hotmail.com>
2020-03-05 22:55:01 +0000

    test

--- app/Http/Controllers/Api/FuelStationController.php
+++ app/Http/Controllers/Api/FuelStationController.php
@@ -49,7 +49,7 @@ class FuelStationController extends Controller
             ->having('distance', '<', $maxDistanceLimit)
             ->orderBy('distance')
             ->whereHas('businessHours', function (Builder $query) {
-                $query->where('business_hours_id', 1);
+                $query->where('day', '=', 'Friday');
             })->get();
 
         // If empty return not found.

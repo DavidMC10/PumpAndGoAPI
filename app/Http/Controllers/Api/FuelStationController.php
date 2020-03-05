04694b598aa094c1e7aec5a91d0d47a4b14cd8b4
David Mc <davidmcelhinney10@hotmail.com>
2020-03-05 21:26:43 +0000

    test

--- app/Http/Controllers/Api/FuelStationController.php
+++ app/Http/Controllers/Api/FuelStationController.php
@@ -53,7 +53,7 @@ class FuelStationController extends Controller
 
         // return $fuelStations;
         // return response()->json(['data' => $fuelStations], 200, [], JSON_NUMERIC_CHECK);
-        return response()->json($date->toDateTimeString());
+        return response()->json($date);
     }
 
     /**

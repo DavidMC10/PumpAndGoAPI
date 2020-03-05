25659c91873d011bc92ab699936e9afe4a7bd486
David Mc <davidmcelhinney10@hotmail.com>
2020-03-05 20:35:17 +0000

    test

--- app/Http/Controllers/Api/FuelStationController.php
+++ app/Http/Controllers/Api/FuelStationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\FuelStation;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -26,6 +27,10 @@ class FuelStationController extends Controller
         $lng = request('longitude');
         $maxDistanceLimit = request('max_distance_limit');
 
+        // Get todays date.
+        $date = Carbon::now();
+        $day =  $date->toArray();
+
         // Query to obtain nearby fuel stations.
         $fuelStations = FuelStation::select(DB::raw('fuel_station_id, name, address1, address2, city_town, telephone_no, number_of_pumps, longitude, latitude,
             ROUND( ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( latitude ) ) * cos(
@@ -33,8 +38,8 @@ class FuelStationController extends Controller
             ->having('distance', '<', $maxDistanceLimit)
             ->orderBy('distance')
             ->with('businessHours:fuel_station_id,business_hours_id,day,open_time,close_time')
+            ->where('day', $day)
             ->get();
-
 
         // If no nearby fuel stations return false.
         if (empty($fuelStations)) {

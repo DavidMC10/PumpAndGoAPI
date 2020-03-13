9421617defc4c82c22e1c3f9a0790ba03f78f96d
David Mc <davidmcelhinney10@hotmail.com>
2020-03-13 21:29:56 +0000

    test

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -152,7 +152,7 @@ class TransactionController extends Controller
         $id = Auth::id();
 
         // Get user transactions.
-        $transactions = User::find($id)->transaction->sortByDesc('transaction_id');
+        $transactions = User::find($id)->transaction->sortBy('transaction_id');
 
         // Get user rewards.
         $rewards = User::find($id)->reward;

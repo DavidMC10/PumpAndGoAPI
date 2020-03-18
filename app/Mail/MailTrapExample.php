04d50cd62d8cafee2c0948b8ddd63104079c5de0
David Mc <davidmcelhinney10@hotmail.com>
2020-03-18 21:38:47 +0000

    test

--- app/Mail/MailTrapExample.php
+++ app/Mail/MailTrapExample.php
@@ -28,7 +28,7 @@ class MailTrapExample extends Mailable
      */
     public function build()
     {
-        return $this->from('mailgun@mg.pumpandgo.net')
+        return $this->from('David mailgun@reply.pumpandgo.net')
         ->subject('Mailtrap Confirmation')
         ->markdown('mails.exmpl')
         ->with([

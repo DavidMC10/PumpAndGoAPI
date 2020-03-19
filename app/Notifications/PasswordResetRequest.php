a7440b67b05b897fe11afea0b9071fe60847a187
David Mc <davidmcelhinney10@hotmail.com>
2020-03-19 12:32:53 +0000

    test

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PasswordResetRequest extends Notification implements ShouldQueue
+class PasswordResetRequest extends Notification
 {
     use Queueable;
 

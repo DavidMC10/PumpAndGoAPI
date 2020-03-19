9bd612add6d60ff6abbf99ce331edca2e2011299
David Mc <davidmcelhinney10@hotmail.com>
2020-03-19 00:18:29 +0000

    test

--- app/PasswordReset.php
+++ app/PasswordReset.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model
 {
+
+    /**
+     * The table name.
+     *
+     * @var
+     */
+    protected $table = 'password_reset';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'email', 'token'
     ];

cd9955c0c755cac46b7899b49991693d4ee80cbe
David Mc <davidmcelhinney10@hotmail.com>
2020-03-19 00:25:46 +0000

    test

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,15 @@ Route::post('login', 'Api\AuthController@login');
 Route::post('register', 'Api\AuthController@register');
 Route::post('refresh', 'Api\AuthController@refresh');
 
-Route::post('create', 'Api\AuthController@create');
-Route::get('find/{token}', 'Api\AuthController@find');
-Route::post('reset', 'Api\AuthController@reset');
+Route::group([
+    'namespace' => 'Auth',
+    'middleware' => 'api',
+    'prefix' => 'password'
+], function () {
+    Route::post('create', 'Api\AuthController@create');
+    Route::get('find/{token}', 'Api\AuthController@find');
+    Route::post('reset', 'Api\AuthController@reset');
+});
 
 Route::middleware('auth:api')->group(function () {
 
